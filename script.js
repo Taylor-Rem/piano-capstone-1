12ec16caed5b0e47ca82e1302e7a0483260e581b
Taylor Remund <remundtaylor3@gmail.com>
2023-01-10 18:03:13 -0700

    piano works good

--- script.js
+++ script.js
@@ -1,58 +1,101 @@
+// decl piano container
 let pianoContainer = document.querySelector('.piano-container');
+let btns = document.querySelectorAll('button');
 
-let synth = new Tone.Synth().toDestination();
+// create synth
+let synth = new Tone.PolySynth(Tone.FMSynth).toDestination();
 
+// create notes
 let notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
-for (let octave = 0; octave < 2; octave++) {
-  for (let i = 0; i < notes.length; i++) {
-    let hasSharp = true;
-    if (notes[i] === 'E' || notes[i] === 'B') {
-      hasSharp = false;
-    }
-    let key = document.createElement('div');
-    key.setAttribute('data-note', notes[i] + (octave + 4));
-    key.classList.add('white', 'key');
-    pianoContainer.appendChild(key);
-    if (hasSharp) {
+// decl start octave
+let startOctave = 4;
+// decl next octave
+let upOctave = startOctave + 1;
+
+// CREATING PIANO
+let createPiano = () => {
+  // number of octaves
+  for (let k = 0; k < 2; k++) {
+    for (let i = 0; i < notes.length; i++) {
+      let hasSharp = true;
+      if (notes[i] === 'E' || notes[i] === 'B') {
+        hasSharp = false;
+      }
+
+      btns[0].addEventListener('click', () => {
+        startOctave--;
+      });
+      btns[1].addEventListener('click', () => {
+        startOctave++;
+      });
+
       let key = document.createElement('div');
-      key.setAttribute('data-note', notes[i] + '#' + (octave + 4));
-      key.classList.add('black', 'key');
+      key.setAttribute('data-note', notes[i] + (k + startOctave));
+      key.classList.add('white', 'key');
       pianoContainer.appendChild(key);
+      if (hasSharp) {
+        let key = document.createElement('div');
+        key.setAttribute('data-note', notes[i] + '#' + (k + startOctave));
+        key.classList.add('black', 'key');
+        pianoContainer.appendChild(key);
+      }
     }
   }
-}
+};
+createPiano();
 let keys = document.querySelectorAll('.key');
 
-// keys.data.note.style = 'rgb(220, 220, 220)';
-
 let keydowns = {
-  z: 'C4',
-  s: 'C#4',
-  x: 'D4',
-  d: 'D#4',
-  c: 'E4',
-  v: 'F4',
-  g: 'F#4',
-  b: 'G4',
-  n: 'A4',
-  m: 'B4',
+  z: 'C' + startOctave,
+  s: 'C#' + startOctave,
+  x: 'D' + startOctave,
+  d: 'D#' + startOctave,
+  c: 'E' + startOctave,
+  v: 'F' + startOctave,
+  g: 'F#' + startOctave,
+  b: 'G' + startOctave,
+  h: 'G#' + startOctave,
+  n: 'A' + startOctave,
+  j: 'A#' + startOctave,
+  m: 'B' + startOctave,
+  q: 'C' + upOctave,
+  2: 'C#' + upOctave,
+  w: 'D' + upOctave,
+  3: 'D#' + upOctave,
+  e: 'E' + upOctave,
+  r: 'F' + upOctave,
+  5: 'F#' + upOctave,
+  t: 'G' + upOctave,
+  6: 'G#' + upOctave,
+  y: 'A' + upOctave,
+  7: 'A#' + upOctave,
+  u: 'B' + upOctave,
 };
 
 keys.forEach((key) => {
-  key.addEventListener('click', () =>
-    synth.triggerAttackRelease(key.dataset.note, '16n')
-  );
+  key.addEventListener('click', () => {
+    synth.triggerAttackRelease(key.dataset.note, '16n');
+    changeColors(key);
+  });
 });
 
 document.addEventListener('keydown', (e) => {
   synth.triggerAttackRelease(keydowns[e.key], '16n');
-  let note = keydowns[e.key];
-  let keyElement = document.querySelector(`[data-note="${note}"]`);
+  changeColors(e);
+});
 
+let changeColors = (e) => {
+  let note;
+  if (e.key) {
+    note = keydowns[e.key];
+  } else {
+    note = e.dataset.note;
+  }
+  let keyElement = document.querySelector(`[data-note="${note}"]`);
   keyElement.style.backgroundColor =
     note.length > 2 ? 'rgb(54, 54, 54)' : 'rgb(220, 220, 220)';
   setTimeout(() => {
     keyElement.style.backgroundColor = note.length > 2 ? 'black' : 'white';
   }, 200);
-});
+};
